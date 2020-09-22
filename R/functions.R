6a68cf531fa1758454be7b9edf19f192384c10ad
marissaashner <mashner@live.unc.edu>
2020-09-22 17:32:49 -0400

    Package Structure/Genome Scan Function and Documentation

---
+++ R/functions.R
@@ -0,0 +1,177 @@
+# this function is used to find the h2 that optimizes the log likelihood
+# h2 is the value that is changed to optimize the log likelihood
+# X is matrix of covariates + SNP
+# y is phenotype vector
+# K is genetic relationship matrix
+# Dhalf is a function of the weight matrix (diagonal matrix of square root of weights)
+# p is the number of strains (make this consistent)
+# laml is the eigenvalues of L
+# Ul is the eigenvectors of L
+# I is a p x p identity
+likeli.brent.het <- function(h2, X, y, K, Dhalf, p, laml, Ul, I){
+  Mhet = diag(1/sqrt(h2*laml + (1-h2)*diag(I))) %*% t(Ul) %*% Dhalf
+  Mx = Mhet %*% X
+  My = Mhet %*% y
+  fit = stats::lm.fit(Mx, c(My))
+  B = fit$coefficients
+  s2 = sum(fit$residuals^2)/p
+
+  # log determinant V
+  LDV = sum(log(diag(Dhalf))) + sum(log(h2*laml + (1-h2)))
+
+  # log likelihood
+  log = -.5*LDV + (-p/2)*log(s2) + (-p/2)*log(2*pi) + (-.5*(p))
+  log
+}
+
+likeli.brent.het.estimates <- function(h2, X, y, K, Dhalf, p, laml, Ul, I){
+  Mhet = diag(1/sqrt(h2*laml + (1-h2)*diag(I))) %*% t(Ul) %*% Dhalf
+  Mx = Mhet %*% X
+  My = Mhet %*% y
+  fit = lm.fit::lm.fit(Mx, c(My))
+  B = fit$coefficients
+  s2 = sum(fit$residuals^2)/p
+
+  list(B = B, s2 = s2)
+}
+
+
+# this function runs a genome scan
+# G is genotype matrix
+# y is phenotype vector
+# X is covariates (or simply, the intercept)
+# K is genetic relationship matrix
+# weight is a vector of the weights
+scan.strain.means <- function(G, y, X, K, weights){
+  # p = number of strains
+  p = dim(K)[1]
+  I = diag(1,p)
+
+  Dhalf = diag(sqrt(weights))
+
+  p_value_het = vector("numeric", p) # p values
+  thetaMLEs = vector("numeric", p) # likelihood under alt
+  theta0s = vector("numeric", p) # likelihood under null
+  s20s = vector("numeric", p) # s2 estimate under null
+  h20s = vector("numeric", p) # h2 estimate under null
+  betas = vector("numeric", p) # beta estimate under null
+
+  # intercept covariate
+  Xint = X
+  # L <- t(t(sqrt(w) * K) * sqrt(w)) --- this is robert's way of finding L, faster?
+  L = Dhalf %*% K %*% Dhalf
+  eL = eigen(L)
+  laml = eL$values
+  Ul = eL$vectors
+  # I = diag(1, p)
+
+  ### RUNNING THE SCAN
+
+  # null scan
+  opt0 = optimize(likeli.brent.het, c(0,1), Xint, y, K, Dhalf, p, laml, Ul, I, maximum = TRUE)
+  theta0 = opt0$objective
+  h20 = opt0$maximum
+
+  #### Extract the parameters
+  params = likeli.brent.het.estimates(h20, Xint, y, K, Dhalf, p, laml, Ul, I)
+  s20 = params$s2 %>% unname() %>% as.numeric()
+  #tau2 = s2*h2_max
+  #sig2 = s2-tau2
+
+
+  # run the single scan for all columns of G
+  #
+  for (i in 1:length(G)){
+    SNP = suppressWarnings(
+      strsplit(str_replace_all(G[i], c("NA"="P", "0.5"="5")),
+               split ="")[[1]]%>%as.numeric())
+    SNP[which(SNP==5)] = 0.5
+    # bind intercept with SNP genotypes
+    X = cbind(Xint, SNP)
+    # check for NAs
+    if (any(is.na(X))){
+      # take out the NA
+      X0 = na.omit(X)
+      # find the indices associated with the NA
+      index = na.omit(X) %>% na.action()
+      # remove those indices from K, y, weights
+      K0 <- K[-c(index), -c(index)]
+      y0 <- y[-c(index)]
+      Dhalf0 <- Dhalf[-c(index), -c(index)]
+      # new number of strains
+      p0 = dim(K0)[1]
+      I0 = diag(1, p0)
+      # re-decompose matrices
+      L0 = Dhalf0 %*% K0 %*% Dhalf0
+      #L0 = (Dhalf0 %*% K0 %*% Dhalf0) %*% t(Z0)
+      eL0 = eigen(L0)
+      laml0 = eL0$values
+      Ul0 =  eL0$vectors
+      # new null scan and SNP scan
+      opt00 = optimize(likeli.brent.het, c(0,1), X0[,1], y0, K0, Dhalf0, p0, laml0, Ul0, I0,
+                       maximum = TRUE)
+      theta00 = opt00$objective
+      h200 = opt00$maximum
+
+      #### Extract the parameters
+      params0 = likeli.brent.het.estimates(h200, X0[,1], y0, K0, Dhalf0, p0, laml0, Ul0, I0)
+      beta000 = params0$B %>% unname() %>% as.numeric()
+      s200 = params0$s2 %>% unname() %>% as.numeric()
+      #tau2 = s2*h2_max
+      #sig2 = s2-tau2
+
+      optMLE = optimize(likeli.brent.het, c(0,1), X0, y0, K0, Dhalf0, p0, laml0, Ul0, I0,
+                        maximum = TRUE)
+      thetaMLE = optMLE$objective
+
+      #### Extract the parameters
+      paramsMLE = likeli.brent.het.estimates(h2, X0, y0, K0, Dhalf0, p0, laml0, Ul0, I0)
+      betas = paramsMLE$B %>% unname() %>% as.numeric()
+      beta10 = betas[2]
+
+      tLR = 2*(thetaMLE - theta00)
+      thetaMLEs[i] = thetaMLE
+      theta0s[i] = theta00
+      s20s[i] = s200
+      h20s[i] = h200
+      betas[i] = beta10
+      p_value_het[i] = pchisq(tLR, 1, lower.tail = FALSE)
+    } else {
+      # SNP scan for no NA
+      optMLE = optimize(likeli.brent.het, c(0,1), X, y, K, Dhalf, p, laml, Ul, I,
+                        maximum = TRUE)
+      thetaMLE = optMLE$objective
+
+      #### Extract the parameters
+      paramsMLE = likeli.brent.het.estimates(h2, X, y, K, Dhalf, p, laml, Ul, I)
+      betas = paramsMLE$B %>% unname() %>% as.numeric()
+      beta1 = betas[2]
+
+      tLR = 2*(thetaMLE - theta0)
+      thetaMLEs[i] = thetaMLE
+      theta0s[i] = theta0
+      s20s[i] = s20
+      h20s[i] = h20
+      betas[i] = beta1
+      p_value_het[i] = pchisq(tLR, 1, lower.tail = FALSE)
+    }
+  }
+  # returns p values
+  list(ps = p_value_het, theta1 = thetaMLEs, theta0 = theta0s,
+       h20 = h20s, s20 = s20s, beta = betas)
+}
+
+### used to find unique snps in genome scan
+mapping = function(x){
+  i = 1
+  while(is.na(x[i])|x[i]==0.5){
+    i = i+1
+  }
+  if(x[i]==1){
+    x = plyr::mapvalues(x, c(0,1), c(1,0), warn_missing = FALSE)
+  }
+  x
+}
+
+
+
---
+++ R/genome_scan.R
@@ -0,0 +1,106 @@
+#' Weighted Genome Scan
+#'
+#' Performs a genome scan on heteroscedastic data.
+#'
+#' @param G A n by p matrix of genotypes, where n is the number of strains and p is the number of snps to be tested (can have missing values)
+#' @param y A n length vector of mean phenotype for each strain
+#' @param noise A n length vector of sample variances for each strain
+#' @param counts A n length vector of number of replicates for each strain
+#' @param X A n by q matrix of covariates (optional)
+#' @param K A n by n genomic relationship matrix. Will be calculated if unspecified.
+#' @param weights A string specifying the weights to be used. The following are permitted: "none", "samplevars", "limma", and "counts"
+#'
+#' @return A list containing:
+#' \itemize{
+#' \item{pvalue: A p length vector of p-values for every snp}
+#' \item{ML0: A p length vector of log maximum likelihood under the null hypothesis}
+#' \item{ML1: A p length vector of log maximum likelihood under the alternative hypothesis}
+#' \item{beta: A p length vector of regression parameter estimate for slope under the alternative hypothesis}
+#' \item{s2: A p length vector of the sum of variance components under the null hypothesis}
+#' \item{h2: A p length vector of the heritability estimate under the null hypothesis}
+#' }
+#'
+#' @import tidyverse
+#' @import stringr
+#' @importFrom limma squeezeVar
+#' @importFrom emma emma.kinship
+#'
+#' @export
+wisam <- function(G, y, noise = NULL, counts = NULL, X, K, weights = "none"){
+
+  # number of strains
+  n <- length(y)
+
+  #### UNACCEPTABLE MISSINGNESS ####
+  if (missing(y)) { stop('Must provide y (n-vector of phenotypes) to run a genome Scan.') }
+  if (missing(G)) { stop('Must provide at least one snp to run a genome scan.')}
+
+  #### ACCEPTABLE MISSINGNESS ####
+  # initialize X to an intercept if missing
+  if (missing(X)) { X <- matrix(data = 1, nrow = n) }
+  # initialize K using the G matrix and emma package if missing
+  if (missing(K)) { K <- emma.kinship(t(G), "additive", "all") }
+
+  #### CONDITIONS THAT CAUSE AN ERROR ####
+  if (!all(c(nrow(X), nrow(G), nrow(K)))) {
+    stop("Input dimensions don't match.")
+  }
+  # checks length of phenotypes, sample variances, and counts, need one for each strain
+  # if noise, counts left NULL, will stop
+  if(!all(c(length(y), length(noise), length(counts)))){
+    stop("Input dimensions don't match.")
+  }
+
+  ## check for strains with 0 variance and take them out
+  if(weights %in% c("samplevars")){
+    ind <- which(noise == 0| counts == 1)
+    if (length(ind) > 0){
+      y <- y[-ind]
+      noise <- noise[-ind]
+      counts <- counts[-ind]
+      K <- K[-ind,-ind]
+      G <- G[-ind,]
+      X <- X[-ind,]
+      print(ind)
+    }
+  }
+
+  ######### WEIGHTS
+  sample_vars = noise
+  if (weights == "samplevars"){
+    print("samplevars")
+    weights = counts/sample_vars
+    weights = weights/sum(weights)*sum(counts)
+  } else if (weights == "limma"){
+    print("limma")
+    vars_shrink_limmar = squeezeVar(sample_vars, counts-1, robust = TRUE)$var.post
+    weights = counts/vars_shrink_limmar
+    weights = weights/sum(weights)*sum(counts)
+  } else if(weights == "none"){
+    print("no weights")
+    weights <- rep(1, dim(K)[1])
+  } else if(weights == "counts"){
+    print("counts")
+    weights <- counts
+    weights = weights/sum(weights)*sum(counts)
+  }
+
+  ######## Find unique SNPs
+  temp = apply(G, 2, mapping)
+  strings = apply(temp, 2, paste0, collapse = "")
+  uniques_indices = tapply(seq_along(strings), strings, identity)[unique(strings)]
+  uniques = unique(strings)
+  unique_counts = lapply(uniques_indices, length) %>% unname() %>% unlist()
+
+  # run the scan (see functions.R)
+  p_value_het = scan.strain.means(uniques, y, X, K, weights)
+    ps = data.frame(indices = uniques_indices %>% unlist %>% unname(),
+                    pvalue = rep(p_value_het$ps, unique_counts),
+                    ML1 = rep(p_value_het$theta1, unique_counts),
+                    ML0 = rep(p_value_het$theta0, unique_counts),
+                    beta = rep(p_value_het$beta, unique_counts),
+                    h2 = rep(p_value_het$h20, unique_counts),
+                    s2 = rep(p_value_het$s20, unique_counts)) %>% arrange(indices) %>%
+      select(-indices) #%>% unlist() %>% unname()
+  ps %>% as.list()
+}
